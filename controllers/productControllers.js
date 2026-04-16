add0ad3f8f5e96fd2ed7df4bb8a94ea7be6e18c4
Vihanga Lahiru <155254835+Vihanga25@users.noreply.github.com>
2026-04-16 17:58:35 +0530

    Add backend files

---
+++ controllers/productControllers.js
@@ -0,0 +1,77 @@
+import Product from "../models/product.js";
+import { isAdmin } from "./userController.js";
+
+export async function createProduct(req, res) {
+    console.log("User data in request:", req.user);
+
+    if (!isAdmin(req)) {
+        return res.status(403).json({ message: "Please log in as an administrator to create a product." });
+    }
+
+    try {
+        const newProduct = new Product(req.body);
+        await newProduct.save();
+        res.json({ message: "Product created", product: newProduct });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+export async function getProducts(req, res) {
+    try {
+        const products = await Product.find({});
+        res.json(products);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+export async function getProductById(req, res) {
+    try {
+        const product = await Product.findOne({ productId: req.params.productId });
+        if (!product) {
+            return res.status(404).json({ message: "Product not found" });
+        }
+        res.json(product);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+export async function deleteProduct(req, res) {
+    if (!isAdmin(req)) {
+        return res.status(403).json({ message: "Please log in as an administrator to delete a product." });
+    }
+
+    try {
+        const deletedProduct = await Product.findOneAndDelete({ productId: req.params.productId });
+
+        if (!deletedProduct) {
+            return res.status(404).json({ message: "Product not found" });
+        }
+
+        res.json({ message: "Product deleted successfully" });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+export async function updateProduct(req, res) {
+    if (!isAdmin(req)) {
+        return res.status(403).json({ message: "Please log in as an administrator to update a product." });
+    }
+
+    try {
+        const updatedProduct = await Product.findOneAndUpdate(
+            { productId: req.params.productId },
+            req.body,
+            { new: true }
+        );
+        if (!updatedProduct) {
+            return res.status(404).json({ message: "Product not found" });
+        }
+        res.json({ message: "Product updated successfully", product: updatedProduct });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
